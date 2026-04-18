ad5589420bbebaaf16c86be5fbf95c465a9f917e
jessicamalta-crypto <jessica.malta@bangtecservices.com>
2026-04-18 15:52:35 -0500

    Add files via upload

---
+++ Description.R
@@ -0,0 +1,7 @@
+#' Suma dos números
+#' @param a Primer número
+#' @param b Segundo número
+#' @export
+suma.dos.numeros <- function(a, b) {
+  return(a + b)
+}
---
+++ suma.R
@@ -0,0 +1,8 @@
+#' Suma dos números
+#' @param a Primer número
+#' @param b Segundo número
+#' @export
+suma.dos.numeros <- function(a, b) {
+  return(a + b)
+}
+
